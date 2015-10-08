2234a59007d37a1255c507419e1c3d922bcebf0a
Sebastian Koall <koall.sebastian@gmail.com>
2015-10-08 16:22:41 +0200

    Add project for tests

---
+++ GitStatisticsAnalyzerUnitTests/GitVersionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitStatisticsAnalyzerUnitTests
+{
+    [TestClass]
+    public class GitVersionTests
+    {
+        [TestMethod]
+        public void TestExistingGitVersion()
+        {
+        }
+
+        public void TestNoGitInstalled()
+        {
+
+        }
+    }
+}
