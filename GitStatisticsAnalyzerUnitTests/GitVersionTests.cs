21f31594f3189acc83b52fa26e8363dccd532654
Sebastian Koall <koall.sebastian@gmail.com>
2015-10-10 09:41:16 +0200

    Add attribute to AssemblyInfo to make classes available in the Tests

--- GitStatisticsAnalyzerUnitTests/GitVersionTests.cs
+++ GitStatisticsAnalyzerUnitTests/GitVersionTests.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using GitStatisticsAnalyzer.Results;
+
 namespace GitStatisticsAnalyzerUnitTests
 {
     [TestClass]
     public class GitVersionTests
     {
+        public GitVersionTests()
+        {
+
+        }
+
         [TestMethod]
         public void TestExistingGitVersion()
         {
+            VersionResult versionResult = new VersionResult();
+            //versionResult.ParseResult();
         }
 
+        [TestMethod]
         public void TestNoGitInstalled()
         {
 
