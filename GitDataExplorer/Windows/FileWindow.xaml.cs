4a47a2633137132886dc15f2bcde2e2a759f7b3c
Sebastian Koall <koall.sebastian@gmail.com>
2017-03-31 16:46:24 +0200

    Add syntax highlighting for files
    supported languages:
    - Java
    - C#
    - Javascript
    - PHP
    - HTML
    - C++

--- GitDataExplorer/Windows/FileWindow.xaml.cs
+++ GitDataExplorer/Windows/FileWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 
 using GitDataExplorer.Commands;
@@ -13,6 +17,16 @@ namespace GitDataExplorer.Windows
     public partial class FileWindow : Window, ICommandWindow
     {
         private IFile file;
+
+        static FileWindow()
+        {
+            FileEndingsHighlighting.Add("Java", new List<string>{ ".java" });
+            FileEndingsHighlighting.Add("C#", new List<string> { ".cs" });
+            FileEndingsHighlighting.Add("JavaScript", new List<string> { ".js" });
+            FileEndingsHighlighting.Add("PHP", new List<string> { ".php" });
+            FileEndingsHighlighting.Add("HTML", new List<string> { ".html", ".htm" });
+            FileEndingsHighlighting.Add("C++", new List<string> { ".cpp", ".cc", ".h" });
+        }
 
         public FileWindow(CommandFactory commandFactory, IFile file)
         {
@@ -41,10 +55,18 @@ namespace GitDataExplorer.Windows
             if (CommandFactory == null) return;
 
             var command = CommandFactory.GetCommand<FileResult>($"{File.CommitId}:{File.Path}");
+            var extension = Path.GetExtension(File.Path);
+            var language = FileEndingsHighlighting.Where(s => s.Value.Contains(extension)).FirstOrDefault().Key;
             command.Execute();
             textEditor.Text = String.Join(Environment.NewLine, command.Result.Lines);
+
+            if (language == null) return;
+
+            textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition(language);
         }
 
         public CommandFactory CommandFactory { get; set; }
+
+        public static readonly Dictionary<string, IList<String>> FileEndingsHighlighting = new Dictionary<string, IList<string>>();
     }
 }
